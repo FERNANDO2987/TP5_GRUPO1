92f3f4b20b44bedaed9fe634bfc89549bd8a9ca0
Fernando2987 <fernandopalacios51@gmail.com>
2024-09-27 23:55:29 -0300

    Add: punto 4-C
    C. El botón aceptar guardará las películas creadas solo si cumplen con las siguientes condiciones: se haya escrito algo sobre el textfield destinado para el nombre de las películas y se haya seleccionado algo diferente a “seleccione un genero” en el combobox. En caso de que no se pueda agregar la película,

--- TP5_GRUPO_1/src/ejercicios/PanelAgregarPelicula.java
+++ TP5_GRUPO_1/src/ejercicios/PanelAgregarPelicula.java
@@ -9,106 +9,146 @@ import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 
 public class PanelAgregarPelicula extends JPanel {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private JTextField textField;
-	private DefaultListModel<Pelicula> listModel;
-	private JComboBox cmbGenero;
+    private static final long serialVersionUID = 1L;
+    private JTextField textField;
+    private DefaultListModel<Pelicula> listModel;
+    private JComboBox<Categoria> cmbGenero;
+    private static int peliculaID = 1;
 
-	/**
-	 * Create the panel.
-	 */
-	public PanelAgregarPelicula() {
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		setLayout(gridBagLayout);
-		
-		JLabel lblNewLabel = new JLabel("ID");
-		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
-		gbc_lblNewLabel.anchor = GridBagConstraints.WEST;
-		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel.gridx = 3;
-		gbc_lblNewLabel.gridy = 2;
-		add(lblNewLabel, gbc_lblNewLabel);
-		
-		JLabel lblNewLabel_2 = new JLabel(String.valueOf(Pelicula.devuelveProximoID()));
-		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
-		gbc_lblNewLabel_2.anchor = GridBagConstraints.WEST;
-		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_2.gridx = 7;
-		gbc_lblNewLabel_2.gridy = 2;
-		add(lblNewLabel_2, gbc_lblNewLabel_2);
-		
-		
-		JLabel lblNewLabel_1 = new JLabel("Nombre");
-		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
-		gbc_lblNewLabel_1.anchor = GridBagConstraints.WEST;
-		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_1.gridx = 3;
-		gbc_lblNewLabel_1.gridy = 4;
-		add(lblNewLabel_1, gbc_lblNewLabel_1);
-		
-		textField = new JTextField();
-		GridBagConstraints gbc_textField = new GridBagConstraints();
-		gbc_textField.insets = new Insets(0, 0, 5, 5);
-		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField.gridx = 7;
-		gbc_textField.gridy = 4;
-		add(textField, gbc_textField);
-		textField.setColumns(10);
-		
-		JLabel lblGenero = new JLabel("Genero");
-		GridBagConstraints gbc_lblGenero = new GridBagConstraints();
-		gbc_lblGenero.insets = new Insets(0, 0, 5, 5);
-		gbc_lblGenero.anchor = GridBagConstraints.WEST;
-		gbc_lblGenero.gridx = 3;
-		gbc_lblGenero.gridy = 6;
-		add(lblGenero, gbc_lblGenero);
-		
-		
-		//Combo Box
-		JComboBox cmbGenero = new JComboBox();
-		GridBagConstraints gbc_comboBox = new GridBagConstraints();
-		gbc_comboBox.insets = new Insets(0, 0, 5, 5);
-		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
-		gbc_comboBox.gridx = 7;
-		gbc_comboBox.gridy = 6;
-		add(cmbGenero, gbc_comboBox);
-		
-		Categoria cat1 = new Categoria(1,"Terror");
-		Categoria cat2 = new Categoria(2,"Accion");
-		Categoria cat3 = new Categoria(3,"Suspenso");
-		Categoria cat4 = new Categoria(4,"Romantica");
-		cmbGenero.addItem(cat1);
-		cmbGenero.addItem(cat2);
-		cmbGenero.addItem(cat3);
-		cmbGenero.addItem(cat4);
-		
-		
-		JButton btnNewButton = new JButton("Aceptar");
-		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-		gbc_btnNewButton.gridwidth = 4;
-		gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
-		gbc_btnNewButton.gridx = 3;
-		gbc_btnNewButton.gridy = 8;
-		add(btnNewButton, gbc_btnNewButton);
+    /**
+     * Create the panel.
+     */
+    public PanelAgregarPelicula() {
+        GridBagLayout gridBagLayout = new GridBagLayout();
+        gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, Double.MIN_VALUE};
+        gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+        setLayout(gridBagLayout);
 
-		
-	}
+        JLabel lblNewLabel = new JLabel("ID");
+        GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+        gbc_lblNewLabel.anchor = GridBagConstraints.WEST;
+        gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+        gbc_lblNewLabel.gridx = 3;
+        gbc_lblNewLabel.gridy = 2;
+        add(lblNewLabel, gbc_lblNewLabel);
 
-	public DefaultListModel<Pelicula> getListModel() {
-		return listModel;
-	}
+        JLabel lblNewLabel_2 = new JLabel(String.valueOf(Pelicula.devuelveProximoID()));
+        GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+        gbc_lblNewLabel_2.anchor = GridBagConstraints.WEST;
+        gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
+        gbc_lblNewLabel_2.gridx = 7;
+        gbc_lblNewLabel_2.gridy = 2;
+        add(lblNewLabel_2, gbc_lblNewLabel_2);
 
-	public void setListModel(DefaultListModel<Pelicula> listModel) {
-		this.listModel = listModel;
-	}
+        JLabel lblNewLabel_1 = new JLabel("Nombre");
+        GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+        gbc_lblNewLabel_1.anchor = GridBagConstraints.WEST;
+        gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+        gbc_lblNewLabel_1.gridx = 3;
+        gbc_lblNewLabel_1.gridy = 4;
+        add(lblNewLabel_1, gbc_lblNewLabel_1);
 
+        textField = new JTextField();
+        GridBagConstraints gbc_textField = new GridBagConstraints();
+        gbc_textField.insets = new Insets(0, 0, 5, 5);
+        gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+        gbc_textField.gridx = 7;
+        gbc_textField.gridy = 4;
+        add(textField, gbc_textField);
+        textField.setColumns(10);
+
+        JLabel lblGenero = new JLabel("Genero");
+        GridBagConstraints gbc_lblGenero = new GridBagConstraints();
+        gbc_lblGenero.insets = new Insets(0, 0, 5, 5);
+        gbc_lblGenero.anchor = GridBagConstraints.WEST;
+        gbc_lblGenero.gridx = 3;
+        gbc_lblGenero.gridy = 6;
+        add(lblGenero, gbc_lblGenero);
+
+        // Combo Box
+        cmbGenero = new JComboBox<>();
+        GridBagConstraints gbc_comboBox = new GridBagConstraints();
+        gbc_comboBox.insets = new Insets(0, 0, 5, 5);
+        gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
+        gbc_comboBox.gridx = 7;
+        gbc_comboBox.gridy = 6;
+        add(cmbGenero, gbc_comboBox);
+
+        // Agregar categorías al combobox
+        Categoria cat1 = new Categoria(1, "Terror");
+        Categoria cat2 = new Categoria(2, "Acción");
+        Categoria cat3 = new Categoria(3, "Suspenso");
+        Categoria cat4 = new Categoria(4, "Romántica");
+        cmbGenero.addItem(cat1);
+        cmbGenero.addItem(cat2);
+        cmbGenero.addItem(cat3);
+        cmbGenero.addItem(cat4);
+
+        JButton btnNewButton = new JButton("Aceptar");
+        GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+        gbc_btnNewButton.gridwidth = 4;
+        gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
+        gbc_btnNewButton.gridx = 3;
+        gbc_btnNewButton.gridy = 8;
+        add(btnNewButton, gbc_btnNewButton);
+
+        // Validar
+        btnNewButton.addActionListener(e -> {
+            if (validarYGuardar()) { 
+                
+                Pelicula nuevaPelicula = new Pelicula(Pelicula.devuelveProximoID(), textField.getText(), (Categoria) cmbGenero.getSelectedItem());
+
+                // Agregar la película
+                if (listModel != null) {
+                    listModel.addElement(nuevaPelicula);
+                }
+
+                // Mostrar mensaje
+                JOptionPane.showMessageDialog(this, "Película agregada exitosamente!", "Confirmación", JOptionPane.INFORMATION_MESSAGE);
+
+             // Incrementar el ID para la próxima película
+				peliculaID++;
+				lblNewLabel_2.setText(String.valueOf(peliculaID)); 
+                // Limpiar los campos
+                textField.setText("");
+                cmbGenero.setSelectedIndex(0);
+            }
+        });
+    }
+
+    // Método de validación 
+    private boolean validarYGuardar() {
+        if (textField == null || cmbGenero == null) {
+            JOptionPane.showMessageDialog(this, "Error: los componentes no se han inicializado correctamente.", "Error", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+
+        String nombrePelicula = textField.getText();
+        Categoria selectedGenero = (Categoria) cmbGenero.getSelectedItem();
+
+        if (nombrePelicula == null || nombrePelicula.isEmpty()) {
+            JOptionPane.showMessageDialog(this, "El nombre de la película no puede estar vacío.", "Error", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+
+        if (selectedGenero == null) {
+            JOptionPane.showMessageDialog(this, "Debe seleccionar un género válido.", "Error", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+
+        return true;
+    }
+
+    public DefaultListModel<Pelicula> getListModel() {
+        return listModel;
+    }
+
+    public void setListModel(DefaultListModel<Pelicula> listModel) {
+        this.listModel = listModel;
+    }
 }
